445218070334e3607dfda69efc399834f6b3c191
michetei2-tech <michetei2@gmail.com>
2026-02-03 11:51:08 +0000

    Deploy updated scoreboard layout

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "857988787"
+    serviceWorkerVersion: "4112122043"
   }
 });
