73af5f43153ec2fd814b0b80e1afb135bc1270e9
michetei2-tech <michetei2@gmail.com>
2026-02-03 11:00:49 +0000

    Deploy old layout

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1277390197"
+    serviceWorkerVersion: "3147647763"
   }
 });
