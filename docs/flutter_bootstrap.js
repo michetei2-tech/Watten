44f8c62d622ef0afedc4a488dc2edf20f4c69684
michetei2-tech <michetei2@gmail.com>
2026-02-05 13:53:13 +0000

    Variante A: Einstellungen -> direkt ZweiGeraeteSetupPage

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2529240963"
+    serviceWorkerVersion: "2346764940"
   }
 });
