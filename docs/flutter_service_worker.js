3fd1d784524526e20f7f5ff38fbb928767aee116
michetei2-tech <michetei2@gmail.com>
2026-02-06 12:45:31 +0000

    Fix: JoinPage session path, final pairing consistency

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c7
 "index.html": "6a66e087703894601c59fb9aa9d3c673",
 "/": "6a66e087703894601c59fb9aa9d3c673",
 "main.dart.js": "1ba9a8666fcf2d7fc40172bb49ad9c82",
-"flutter_bootstrap.js": "e62c714a2030fa53b93a47ccfff1159c",
+"flutter_bootstrap.js": "1012efa36a67cfd64afbf1dd72f1fd19",
 "version.json": "322dfa94430cc69aa45ce83495f46466",
 "manifest.json": "8e66e1314dbabf0aa13062137d7bf013",
 "_redirects": "5f52dbefcf63c9f677ace09fcf8bc7d8",
