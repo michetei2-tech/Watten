ee79ce7be3799bba60ce87a3e4d33a45744e95fe
michetei2-tech <michetei2@gmail.com>
2026-02-13 11:50:53 +0000

    Deploy: anpassungen 3

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3251313215"
+    serviceWorkerVersion: "1653277020"
   }
 });
