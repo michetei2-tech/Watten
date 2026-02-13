be56d38b96f0b2bb06d674e9e498fad5c0c1676d
michetei2-tech <michetei2@gmail.com>
2026-02-13 13:39:18 +0000

    Deploy: anpassungen 3

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c7
 "index.html": "6a66e087703894601c59fb9aa9d3c673",
 "/": "6a66e087703894601c59fb9aa9d3c673",
 "main.dart.js": "4ef6fe4ac923b3ad8d2dcd4dd4e7888b",
-"flutter_bootstrap.js": "2b2057db03ba88678033e017d562f1dc",
+"flutter_bootstrap.js": "1cd88494ec4eab0c9ba269e9787bd6a3",
 "version.json": "322dfa94430cc69aa45ce83495f46466",
 "manifest.json": "8e66e1314dbabf0aa13062137d7bf013",
 "_redirects": "5f52dbefcf63c9f677ace09fcf8bc7d8",
